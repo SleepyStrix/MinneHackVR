241270cf0ad6a8460c5d4c6fcd479008910c59a9
Daniel Hewlett <DanCHewlett@gmail.com>
2015-10-25 01:04:17 -0400

    Initial Commit

---
+++ Assets/Scripts/AlwaysFaceTarget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlwaysFaceTarget : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 newFaceTarg = new Vector3((180 - target.position.x), (180 - target.position.y), (180- target.position.z));
+		transform.LookAt (newFaceTarg);
+	}
+}
